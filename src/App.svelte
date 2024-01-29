95622ff0359c77a3c46714977def605620705404
CVakratsas <c.vakratsas@gmail.com>
2024-01-29 20:21:41 +0200

    Components, styles and local Prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,35 @@
 <script>
-	let firstName = 'Costas';
-	let lastName = 'Vak';
-	let beltColour = 'black';
+	import Modal from './Modal.svelte';
 
-	$: fullName = `${firstName} ${lastName}`;
-	$: {
-		console.log(beltColour);
-		console.log(fullName);
-	}
+	let people = [
+    { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+    { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+    { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+  ];
 
-	const handleClick = () => {
-		beltColour = 'orange';
-	}
+  const handleClick = (id) => {
+	people = people.filter((person) => person.id != id)
+  }
+
+  let num = 5;
 </script>
 
+<Modal />
+
+
 <main>
-	<p>{fullName} - {beltColour} belt</p>
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<input type="text" bind:value={beltColour}>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColour === 'black'}
+				<p><strong>MASTER NINJA</strong></p>
+			{/if}
+			<p>{person.age} years old, {person.beltColour} belt</p>
+			<button on:click={() => handleClick(person.id)}>Delete</button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
 </main>
 
 <style>
